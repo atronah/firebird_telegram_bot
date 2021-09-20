148b16adf1471f3a26affa50e801ec71864c7734
atronah <atronah.ds@gmail.com>
2021-09-20 03:31:51 +0300

    fixup! ft(tbl_subscription): adds table for info about subscriptions to the commands

--- scripts/tbl_fbtb_command_subscription.sql
+++ scripts/tbl_fbtb_command_subscription.sql
@@ -4,11 +4,11 @@ create table fbtb_command_subscription(
     subscription_id bigint
     , command_id bigint
     , chat_id bigint
+    , repeat_after varchar(32)
     , start_date date
     , end_date date
     , from_time time
     , to_time time
-    , repeat_after varchar(32)
     , no_sound smallint
     , constraint pk_fbtb_command_subscription primary key (subscription_id)
 );
