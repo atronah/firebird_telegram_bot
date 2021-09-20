3fc45ede23e093f2df74e0b9fc966297fb49bb90
atronah <atronah.ds@gmail.com>
2021-09-20 12:33:31 +0300

    fixup! fixup! ft(get_response_handler): adds procedure to handle responses from Telegram API

--- scripts/prc_fbtb_response_handler.sql
+++ scripts/prc_fbtb_response_handler.sql
@@ -7,8 +7,14 @@ create or alter procedure fbtb_response_handler(
 as
 declare bot_id type of column fbtb_bot.bot_id;
 
-declare status type of column fbtb_command_request.status = 0;
+declare request_id type of column fbtb_command_request.request_id;
+declare from_chat_id type of column fbtb_command_request.from_chat_id;
+declare from_user_id type of column fbtb_command_request.from_user_id;
 declare request_text type of column fbtb_command_request.request_text;
+
+declare from_chat_title type of column fbtb_chat.title;
+declare from_user_name type of column fbtb_user.username;
+
 declare command_name type of column fbtb_command.command_name;
 
 declare message_data blob sub_type text;
@@ -21,10 +27,16 @@ declare SENT type of column fbtb_command_request.status = 2;
 declare SKIPPED type of column fbtb_command_request.status = 3;
 declare FAILED type of column fbtb_command_request.status = 4;
 begin
-    -- processing send Messages responses:
+    -- processing sendMessages responses:
     if (request_id > 0) then
     begin
+        update fbtb_command_request
+                set status = :SENT
+                    , status_updated = 'now'
+                    , result_response = :response
+            where request_id = :request_id;
     end
+    -- processing getUpdates responses:
     else if (request_id < 0) then
     begin
         bot_id = -request_id;
@@ -37,74 +49,28 @@ begin
             into message_data
         do
         begin
-            chat_id = null; request_text = null;
-
-            status = UNPROCESSED;
-
+            from_chat_id = null; from_chat_title = null;
+            from_user_id = null; from_user_name = null;
+            request_text = null;
             select
-                    max(iif(path = '/-/chat/' and name = 'id', val, null) as chat_id
-                    , max(iif(path = '/-/' and name = 'text', val, null) as request_text
+                    max(iif(path = '/-/chat/' and name = 'id', val, null)) as from_chat_id
+                    , max(iif(path = '/-/chat/' and name = 'title', val, null)) as from_chat_title
+                    , max(iif(path = '/-/from/' and name = 'id', val, null)) as from_user_id
+                    , max(iif(path = '/-/from/' and name = 'username', val, null)) as from_user_name
+                    , max(iif(path = '/-/' and name = 'text', val, null)) as request_text
                 from aux_json_parse(:message_data)
-                into chat_id, request_text;
+                into from_chat_id, from_chat_title
+                        , from_user_id, from_user_name
+                        , request_text;
 
-            if (request_text starts with '/') then
-            begin
+            select request_id
+                from fbtb_process_command_request(:bot_id, :request_text, :from_chat_id, :from_user_id)
+                into request_id;
 
-                select
-                        allowed_chat_id_list
-                        , allowed_user_id_list
-                        , result_statement
-                    from fbtb_command as c
-                    where c.bot_id = :bot_id
-                        and c.command_name = :command_name
-            end
+            update or insert into fbtb_chat(chat_id, title, updated) value(:from_chat_id, :from_chat_title, 'now');
+            update or insert into fbtb_user(user_id, username, updated) value(:from_user_id, :from_user_name, 'now');
         end
     end
-
-    for select
-            r.request_id
-            , r.result_text
-            , r.url
-            , r.http_method
-        from fbtb_command_request as r
-            left join fbtb_command_subscription as s using(subscription_id)
-        where r.status = :UNPROCESSED
-            and (s.subscription_id is null
-                or current_timestamp between
-                    coalesce(s.start_date, current_date) + coalesce(s.from_time, cast(current_timestamp as time))
-                        and coalesce(s.end_date, current_date) + coalesce(s.to_time, cast(current_timestamp as time))
-            )
-            into request_id, result_text, url, http_method
-    do
-    begin
-        suspend;
-    end
-
-    for select
-            b.bot_id
-            , null as result_text
-            , 'https://api.telegram.org/bot'|| b.bot_token || '/getUpdates' as url
-            , 'GET' as http_method
-            , b.bot_id
-            , coalesce(b.messages_per_day_limit, 0)
-        from fbtb_bot as b
-        where b.enabled > 0
-            and dateadd(b.update_delay_in_milliseconds millisecond to b.last_updates) < current_timestamp
-        into bot_id, result_text, url, http_method, messages_per_day_limit
-    do
-    begin
-        request_id = -bot_id;
-
-        if (messages_per_day_limit = 0
-                or messages_per_day_limit > (select count(distinct r.request_id)
-                                                from fbtb_command as c
-                                                    inner join fbtb_command_request as r using(command_id)
-                                                where c.bot_id = :bot_id
-                                                    and r.status = :SENT
-                                                    and cast(r.status_updated as date) = current_date)
-        ) then suspend;
-    end
-
 end^
 
 set term ; ^
