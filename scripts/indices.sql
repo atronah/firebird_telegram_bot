c5097d4f9555ed3bf543af4c2595394ee3ae0974
atronah <atronah.ds@gmail.com>
2021-09-20 03:32:36 +0300

    ft(indices): adds indices for `allowed_*_id_list` fields of `fbtb_command`

---
+++ scripts/indices.sql
@@ -0,0 +1,2 @@
+create index idx_fbtb_command_a_chat_list on fbtb_command computed by (coalesce(allowed_chat_id_list, ''));
+create index idx_fbtb_command_a_user_list on fbtb_command computed by (coalesce(allowed_user_id_list, ''));
